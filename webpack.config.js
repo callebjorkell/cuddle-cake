7c6dda8bf4b0292db75f5d05fb996f7af18a8cef
Nicolai Willems <nwi@tradeshift.com>
2017-09-28 11:58:10 +0200

    Add node app with bells and whistles

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       },
+       { test: /\.(eot|svg|ttf|woff|woff2)$/, use: ['file-loader'] } 
+     ]
+   }
+};
